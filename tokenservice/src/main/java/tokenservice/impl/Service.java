008627da909e01b48557f268567eb79a40db08fe
Lukas Åkefeldt <lukas.akefeldt@gmail.com>
2025-01-16 12:54:55 +0100

    tokenservices

--- tokenservice/src/main/java/tokenservice/impl/Service.java
+++ tokenservice/src/main/java/tokenservice/impl/Service.java
@@ -3,40 +3,33 @@ package tokenservice.impl;
 import tokenservice.lib.IRepository;
 import tokenservice.lib.IService;
 import messaging.Event;
-import messaging.MessageQueue;
 import java.util.Stack;
+import tokenservice.adaptors.*;
 
 public class Service implements IService {
 	private final IRepository repository;
-	private final MessageQueue queue;
+	private final EventPublisher publisher;
 	private final TokenGenerator tokenGenerator;
 
-	public Service(MessageQueue q, IRepository repo, TokenGenerator tokenGenerator) {
-		this.queue = q;
+	public Service(EventPublisher p, IRepository repo, TokenGenerator tokenGenerator) {
+		this.publisher = p;
 		this.repository = repo;
 		this.tokenGenerator = tokenGenerator;
-		this.queue.addHandler("generate.tokens.requested", this::handleTokensRequested);
-		this.queue.addHandler("find.id.by.token.requested", this::handleTokenIdRequested);
-		this.queue.addHandler("token.is.used", this::handleTokenUsed);
+
 	}
 
 	@Override
-	public void handleTokensRequested(Event ev) {
-		var id = ev.getArgument(1, String.class);
-		var correlationId = ev.getArgument(0, CorrelationId.class);
+	public void handleTokensRequested(String id, CorrelationId correlationId) {
 
 		Stack<String> tks = new Stack<String>();
 		try {
 			tks = tokenGenerator.generateTokens();
 			System.out.println("tokens: " + tks);
 			repository.addTokens(id, tks);
-			ev = new Event("tokens.generate.succeeded", new Object[] { correlationId, tks });
+			publisher.emitTokenGenerated(correlationId);
 		} catch (Exception e) {
-			ev = new Event("tokens.generate.failed", new Object[] { correlationId, e });
+			publisher.emitTokenGenerated(correlationId);
 		}
-
-		// Publish the event
-		queue.publish(ev);
 	}
 
 	public void handleTokenIdRequested(Event ev) {
@@ -49,8 +42,6 @@ public class Service implements IService {
 			ev = new Event("find.id.by.token.failed", new Object[] { correlationId, e });
 		}
 
-		// Publish the event
-		queue.publish(ev);
 	}
 
 	public void handleTokenUsed(Event ev) {
