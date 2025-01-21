9e480d343a230c64fd791f666422bb8ea275231e
zouge666 <1615379639@qq.com>
2025-01-22 00:35:20 +0100

    feat/unregister-1

--- account-management/src/main/java/micro/commands/AccountDeletionCommand.java
+++ account-management/src/main/java/micro/commands/AccountDeletionCommand.java
@@ -4,6 +4,10 @@ public class AccountDeletionCommand {
     private String accountId;
 
     public AccountDeletionCommand(String accountId) {
-        
+        this.accountId = accountId;
+    }
+
+    public String getAccountId() {
+        return accountId;
     }
 }
--- account-management/src/main/java/micro/service/AccountManagementService.java
+++ account-management/src/main/java/micro/service/AccountManagementService.java
@@ -77,5 +77,7 @@ public class AccountManagementService {
 
 	public void handleDeleteAccount(AccountDeletionCommand command, CorrelationId correlationId) {
 
+
+
 	}
 }
