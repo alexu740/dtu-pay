0401be31fcd0836336c7560b6265059ebb4c07c3
nveniz <nveniz@users.noreply.github.com>
2025-01-23 16:50:22 +0100

    Removed unused code/imports

--- tokenservice/src/main/java/tokenservice/startup/ApplicationFactory.java
+++ tokenservice/src/main/java/tokenservice/startup/ApplicationFactory.java
@@ -1,8 +1,6 @@
 package tokenservice.startup;
 
-import tokenservice.lib.IRepository;
 import tokenservice.service.TokenService;
-import tokenservice.impl.Repository;
 import tokenservice.adapters.*;
 import boilerplate.MessageQueue;
 import boilerplate.implementations.RabbitMqQueue;
@@ -10,7 +8,6 @@ import boilerplate.implementations.RabbitMqQueue;
 public class ApplicationFactory {
     public static RabbitMqFacade createApplication() {
         MessageQueue mq = new RabbitMqQueue("rabbitMq");
-        IRepository repo = new Repository();
         EventPublisher publisher = new RabbitMqEventPublisher(mq);
         var service = new TokenService(publisher);
         var facade = new RabbitMqFacade(mq, service);
